abc17e0d27640415f819ef50c2f80579e3514ad6
etchepared <88214684+etchepared@users.noreply.github.com>
2022-03-29 15:14:38 -0300

    comentario de prueba

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+//Esto es un comentario
 function App() {
   return (
     <div className="App">
