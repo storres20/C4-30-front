6f79b3c5f376b00a72799c0f1c16d9f9863b0bd1
Rosamaria Rodriguez <rous9716@gmail.com>
2022-03-30 19:53:52 -0500

    Creacion: Archivo y estructura base para la vistas según roles(Visitante, Usuario y Admin)

---
+++ src/componentes/vistas/vistaAdmin/VistaAdmin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VistaAdmin() {
+  return (
+    <div>VistaAdmin</div>
+  )
+}
+
+export default VistaAdmin
---
+++ src/componentes/vistas/vistaVisitante/VistaVisitante.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VistaVisitante() {
+  return (
+    <div>VistaVisitante</div>
+  )
+}
+
+export default VistaVisitante
