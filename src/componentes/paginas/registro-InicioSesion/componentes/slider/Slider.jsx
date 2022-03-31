1c6e3717e9d1cc1fbdc27c30d820b84328dffe80
Rosamaria Rodriguez <rous9716@gmail.com>
2022-03-30 19:49:27 -0500

    Creacion: Carpetas con archivos base para el slider

---
+++ src/componentes/paginas/registro-InicioSesion/componentes/slider/Slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Slider(props) {
+  return (
+    <div>Slider</div>
+  )
+}
+
+Slider.propTypes = {}
+
+export default Slider
