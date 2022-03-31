d68c7976f918fb860cdfde654aa316daba5227cd
Rosamaria Rodriguez <rous9716@gmail.com>
2022-03-31 12:32:44 -0500

    Estilo: Agregar la funcionalidad al contenedor style

--- src/componentes/paginas/registro-InicioSesion/componentes/slider/Slider.jsx
+++ src/componentes/paginas/registro-InicioSesion/componentes/slider/Slider.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import "./Slider.scss";
+
+
+
 
 function Slider() {
-  return (
-    <section>
+  const colors = ["#CE972B", "#336990", "#33907C", "#EB5757"];
+  const delay = 2500;
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
 
-    </section>
-  )
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+    
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === colors.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  });
+  return (
+    <div className="slideshow">
+      <div
+        className="slideshowSlider"
+        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+      >
+        {colors.map((backgroundColor, index) => (
+          <div className="slide" key={index} style={{ backgroundColor }}></div>
+        ))}
+      </div>
+
+      <div className="slideshowDots">
+        {colors.map((_, idx) => (
+          <div
+            key={idx}
+            className={`slideshowDot${index === idx ? " active" : ""}`}
+            onClick={() => {
+              setIndex(idx);
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-Slider.propTypes = {}
+Slider.propTypes = {};
 
-export default Slider
+export default Slider;
