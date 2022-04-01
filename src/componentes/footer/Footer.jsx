082502fcca9df5d1d779481c52303beb68accf09
storres20 <sanchotorres777@gmail.com>
2022-03-31 21:06:08 -0500

    starting footer desktop view

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -16,22 +16,47 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='style'>
-      <div className='divcontactanos'>
-        <h1 className='title'>Contáctanos</h1>
-        <div className='flex'>
-          <img className='icon' src={phone} alt="phone" />
-          <h2 className='subtitle'>(480) 555-0103</h2>
+      <div className='flex'>
+        <div className='flex hide-phone'>
+          <div>
+            <h1 className='title'>Contenido</h1>
+            <h2 className='subtitle'>Conocenos</h2>
+            <h2 className='subtitle'>Nuestra Carta</h2>
+            <h2 className='subtitle'>Contactanos</h2>
+          </div>
+
+          <div>
+            <h1 className='title'>Unete</h1>
+            <h2 className='subtitle'>Inicia Sesion</h2>
+            <h2 className='subtitle'>Registrate</h2>
+          </div>
+
+          <div>
+            <h1 className='title'>Sucursales</h1>
+            <h2 className='subtitle'>Lores Ipsum</h2>
+            <h2 className='subtitle'>Lores Ipsum</h2>
+            <h2 className='subtitle'>Lores Ipsum</h2>
+            <h2 className='subtitle'>Lores Ipsum</h2>
+          </div>
         </div>
-        <div className='flex'>
-          <img className='icon' src={map} alt="map" />
-          <h2 className='subtitle'>4517 Washington Ave.</h2>
-        </div>
-        <div className='flex'>
-          <img className='icon' src={send} alt="send" />
-          <h2 className='subtitle'>debra.holt@example.com</h2>
+
+        <div className='divcontactanos'>
+          <h1 className='title'>Contáctanos</h1>
+          <div className='flex'>
+            <img className='icon' src={phone} alt="phone" />
+            <h2 className='subtitle'>(480) 555-0103</h2>
+          </div>
+          <div className='flex'>
+            <img className='icon' src={map} alt="map" />
+            <h2 className='subtitle'>4517 Washington Ave.</h2>
+          </div>
+          <div className='flex'>
+            <img className='icon' src={send} alt="send" />
+            <h2 className='subtitle'>debra.holt@example.com</h2>
+          </div>
         </div>
       </div>
-      
+
       <div>
         <h1 className='title'>Redes sociales</h1>
         <div className='flex'>
