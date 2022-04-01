958475ab07ef11d8a0f3d2f588375743dec4645d
storres20 <sanchotorres777@gmail.com>
2022-03-31 19:49:56 -0500

    footer - contactanos

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -4,22 +4,28 @@ import phone from "../../imagenes/iconos/bx_bx-phone.svg"
 import map from "../../imagenes/iconos/bx_bx-map.svg"
 import send from "../../imagenes/iconos/carbon_send-alt.svg"
 
+import './Footer.css'
+
 export default function Footer() {
   return (
-    <div>
-      <h1>Contáctanos</h1>
+    <div className='style'>
       <div>
-        <img src={phone} alt="phone" />
-        <h2>(480) 555-0103</h2>
+        <h1 className='title'>Contáctanos</h1>
+        <div className='flex'>
+          <img className='icon' src={phone} alt="phone" />
+          <h2 className='subtitle'>(480) 555-0103</h2>
+        </div>
+        <div className='flex'>
+          <img className='icon' src={map} alt="map" />
+          <h2 className='subtitle'>4517 Washington Ave.</h2>
+        </div>
+        <div className='flex'>
+          <img className='icon' src={send} alt="send" />
+          <h2 className='subtitle'>debra.holt@example.com</h2>
+        </div>
       </div>
-      <div>
-        <img src={map} alt="map" />
-        <h2>4517 Washington Ave.</h2>
-      </div>
-      <div>
-        <img src={send} alt="send" />
-        <h2>debra.holt@example.com</h2>
-      </div>
+      
+      
     </div>
   )
 }
