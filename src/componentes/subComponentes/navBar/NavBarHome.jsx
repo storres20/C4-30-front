a2221202025c798913770230474c9533a49dd5a5
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2022-04-03 18:19:54 -0300

    Inserción de estilos

--- src/componentes/subComponentes/navBar/NavBarHome.jsx
+++ src/componentes/subComponentes/navBar/NavBarHome.jsx
@@ -1,65 +1,44 @@
 import React from "react";
-import logo from '../../imagenes/LogoVeride/LogosSinFondo/Logo3-Largo.png';
+import logo from '../../../imagenes/LogoVeride/LogosSinFondo/Logo3-Largo.png';
+import "./NavBar.scss"
 
-// const isLoggedIn = false;
-// const isAdmin = false;
+const isLoggedIn = false;
 
 export default function NavBarHome() {
     return (
     <nav>
-        <div>
+        <div className="navbar-left">
             <img src={logo} alt="Veride" />
-            <a href="/">Productos</a>
-            <a href="/sobre-veride">Sobre Veridē</a>
+            <a className="productos" href="/">Productos</a>
+            <a className="sobre-veride" href="/sobre-veride">Sobre Veridē</a>
         </div>
-        <div>
-            <form>
+        <div className="navbar-right">
+            <form class="form-inline">
                 <input type="text" placeholder="Buscar..." />
                 <button type="submit">
                     <i class="uil uil-search"></i>
                 </button>
             </form>
-            <a href="/carrito">
+            <a className="carrito" href="/carrito">
                 <i class="uil uil-shopping-cart"></i>
             </a>
-            {/* {isLoggedIn ?
-                <div>
-                    {!isAdmin ?
-                        <div>
-                            <a href="/cuenta">nombre usuario</a>
-                            <ul>
-                                <li>
-                                    <a href="configuración-de-cuenta">Configuración de Cuenta</a>
-                                </li>
-                                <li>
-                                    <a href="historial-de-compras">Historial de compras</a>
-                                </li>
-                                <button>
-                                    <i class="uil uil-signout"></i>
-                                </button>
-                            </ul>
-                        </div>
-                    :
-                        <div>
-                            <a href="/cuenta">nombre admin</a>
-                            <ul>
-                                <li>
-                                    <a href="configuración-de-cuenta">Configuración de Cuenta</a>
-                                </li>
-                                <li>
-                                    <a href="historial-de-ventas">Historial de ventas</a>
-                                </li>
-                                <li>
-                                    <a href="editar-productos">Editar productos</a>
-                                </li>
-                                <button>
-                                    <i class="uil uil-signout"></i>
-                                </button>
-                            </ul>
-                        </div>}
-                </div>
+            {isLoggedIn ?
+                    <div>
+                        <a href="/cuenta">nombre usuario</a>
+                        <ul>
+                            <li>
+                                <a href="configuración-de-cuenta">Configuración de Cuenta</a>
+                            </li>
+                            <li>
+                                <a href="historial-de-compras">Historial de compras</a>
+                            </li>
+                            <button>
+                                <i class="uil uil-signout"></i>
+                            </button>
+                        </ul>
+                    </div>
             :
-                <a href="/inicio-sesion">Ingresa aquí</a>} */}
+                <a className="ingreso" href="/inicio-sesion">Ingresa aquí</a>}
         </div>        
     </nav>
     );
