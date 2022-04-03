2220bf4a9f9a6c25fe5578804afe930a333e90ae
storres20 <sanchotorres777@gmail.com>
2022-04-03 00:42:34 -0500

    add onclick on heart and bag to change state

--- src/componentes/subComponentes/vistaprodhome/Vistaprodhome.jsx
+++ src/componentes/subComponentes/vistaprodhome/Vistaprodhome.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Vistaprodhome.css'
 
 import imagen from "./imagenes/img.svg"
 import bag from "./imagenes/bag.svg"
+import bagbold from "./imagenes/bagbold.svg"
 import heart from "./imagenes/heart.svg"
+import heartbold from "./imagenes/heartbold.svg"
 import clock from "./imagenes/clock.svg"
 import burger from "./imagenes/burger.svg"
 import pizza from "./imagenes/pizza.svg"
 
 export default function Vistaprodhome() {
+
+  const [isHeart, setIsHeart] = useState(false)
+  const buttonHeart = () => {
+    setIsHeart(current => !current)
+  }
+  
+  const [isBag, setIsBag] = useState(false)
+  const buttonBag = () => {
+    setIsBag(current => !current)
+  }
+
   return (
     <div className='contenedorVPH'>
       <img src={imagen} alt="imagen" />
@@ -18,8 +31,9 @@ export default function Vistaprodhome() {
         <div className='flex1'>
           <h1>Burgers & Pizza</h1>
           <div>
-            <img className='iconheart' src={heart} alt="heart" />
-            <img className='iconbag' src={bag} alt="bag" />
+            <img className='iconheart' src={isHeart ? heartbold : heart} alt="heart" onClick={buttonHeart} />
+            <img className='iconbag' src={isBag ? bagbold : bag} alt="bag" onClick={buttonBag} />
+            
           </div>
         </div>
         
