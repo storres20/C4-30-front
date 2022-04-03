1031ef0dc8a1020faad44de15da16620fdebd9d5
KaterinTello <u.kate.tello@gmail.com>
2022-04-02 20:38:11 -0500

    feat: archivo de la vista Sobre Verede

---
+++ src/componentes/paginas/SobreVerede.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './registro-InicioSesion/componentes/NavBar'
+import {BrowserRouter as Router} from 'react-router-dom'
+
+function SobreVerede() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  )
+}
+
+export default SobreVerede
