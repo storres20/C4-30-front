6e8b7cac52337fa55e374fb4a5a1c50964d5fa1a
KaterinTello <u.kate.tello@gmail.com>
2022-04-03 05:22:56 -0500

    componente padre recibe nuevas secciones del landing page

--- src/componentes/paginas/SobreVerede.jsx
+++ src/componentes/paginas/SobreVerede.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import NavBar from './registro-InicioSesion/componentes/NavBar'
 import {BrowserRouter as Router} from 'react-router-dom'
+import Sidebar from './registro-InicioSesion/componentes/Sidebar/Index'
+import HeroSection from './registro-InicioSesion/componentes/HeroSection';
+import InfoSection from './registro-InicioSesion/componentes/InfoSection';
+import Services from './registro-InicioSesion/componentes/Services';
+import Reconocimiento from './registro-InicioSesion/componentes/Reconocimiento';
+import Sucursales from './registro-InicioSesion/componentes/Sucursales';
 
 function SobreVerede() {
+  const [isOpen, setIsOpen] = useState(false);
+
+const toggle = () => {
+  setIsOpen(!isOpen)
+}
+
   return (
     <div>
-      <NavBar />
+      <Sidebar isOpen={isOpen} toggle= {toggle}/>
+      <NavBar toggle= {toggle}/>
+      <HeroSection />
+      <InfoSection />
+      <Services />
+      <Reconocimiento />
+      <Sucursales />
     </div>
   )
 }
