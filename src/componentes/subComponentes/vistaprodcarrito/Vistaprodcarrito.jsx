0404b1229d38e0d31f9bb1e1775594882e030d5e
storres20 <sanchotorres777@gmail.com>
2022-04-03 19:14:01 -0500

    add Vistaprodcarrito.jsx

---
+++ src/componentes/subComponentes/vistaprodcarrito/Vistaprodcarrito.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Vistaprodcarrito.css'
+
+import VPC from './VPC/VPC'
+
+export default function Vistaprodcarrito() {
+  return (
+    <div className='flex0'>
+      {/* Estos VPH luego pueden ser mapeados con la data de la API */}
+      <VPC/>
+      <VPC/>
+      <VPC/>
+      <VPC/>
+      <VPC/>
+      <VPC/>
+    </div>
+  )
+}
