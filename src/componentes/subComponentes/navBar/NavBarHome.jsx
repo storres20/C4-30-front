a2e7d353fc8d05d3740a366a7dace7573e71d0c9
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2022-04-07 16:36:54 -0300

    Navbar responsiveness

--- src/componentes/subComponentes/navBar/NavBarHome.jsx
+++ src/componentes/subComponentes/navBar/NavBarHome.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from '../../../imagenes/LogoVeride/LogosSinFondo/Logo3-Largo.png';
 import "./NavBar.scss"
 
-const isLoggedIn = false;
+const isLoggedIn = true;
 
 export default function NavBarHome() {
     return (
@@ -15,7 +15,7 @@ export default function NavBarHome() {
         <div className="navbar-right">
             <form class="form-inline">
                 <input type="text" placeholder="Buscar..." />
-                <button type="submit">
+                <button type="submit" className="boton-buscar">
                     <i class="uil uil-search"></i>
                 </button>
             </form>
@@ -23,22 +23,36 @@ export default function NavBarHome() {
                 <i class="uil uil-shopping-cart"></i>
             </a>
             {isLoggedIn ?
-                    <div>
-                        <a href="/cuenta">nombre usuario</a>
-                        <ul>
-                            <li>
-                                <a href="configuración-de-cuenta">Configuración de Cuenta</a>
-                            </li>
-                            <li>
-                                <a href="historial-de-compras">Historial de compras</a>
-                            </li>
-                            <button>
-                                <i class="uil uil-signout"></i>
-                            </button>
-                        </ul>
+                    <div className="logged-dropdown">
+                        <i class="uil uil-ellipsis-v"></i>
+                        <div className="logged-mobile-dropdown-content">
+                            <a className="link-usuario" href="/cuenta">usuario</a>
+                            <a className="productos" href="/">Productos</a>
+                            <a className="sobre-veride" href="/sobre-veride">Sobre Veridē</a>
+                            <a href="configuracion-de-cuenta">Configuración de Cuenta</a>
+                            <a href="historial-de-compras">Historial de Compras</a>
+                            <a href="logout">Cerrar Sesión<i class="uil uil-signout"></i></a>
+                        </div>
+                        
+                        <a href="/cuenta" className="cuenta">nombre usuario<i class="uil uil-angle-down"></i></a>
+                        <div className="logged-desktop-dropdown-content">
+                            <a href="configuracion-de-cuenta">Configuración de Cuenta</a>
+                            <a href="historial-de-compras">Historial de Compras</a>
+                            <a href="logout">Cerrar Sesión<i class="uil uil-signout"></i></a>
+                        </div>
                     </div>
             :
-                <a className="ingreso" href="/inicio-sesion">Ingresa aquí</a>}
+                <div className="ingreso-container">
+                    <a className="ingreso" href="/inicio-sesion"><span>Ingresa aquí</span></a>
+                    <div className="unlogged-dropdown">
+                        <a href="/cuenta" className="cuenta-ingreso"><i class="uil uil-ellipsis-v"></i></a>
+                        <div className="unlogged-dropdown-content">
+                            <a className="ingreso2" href="/inicio-sesion">Ingresa aquí</a>
+                            <a className="productos" href="/">Productos</a>
+                            <a className="sobre-veride" href="/sobre-veride">Sobre Veridē</a>
+                        </div>
+                    </div>
+                </div>}           
         </div>        
     </nav>
     );
