bb85c17c2d3db44b83276e91e73bb2088cedaaec
Rosamaria Rodriguez <rous9716@gmail.com>
2022-04-11 19:51:00 -0500

    Add: Footer and Nav for 'configuracion Cuuenta' view

--- src/componentes/paginas/configuracionCuenta/contenedores/DireccionUser.jsx
+++ src/componentes/paginas/configuracionCuenta/contenedores/DireccionUser.jsx
@@ -3,12 +3,18 @@ import "./ConfiguracionCuenta.scss";
 import "antd/dist/antd.css";
 import SidebarLateral from "../componentes/SidebarLateral";
 import FormDireccionUser from "../componentes/formularios/FormDireccionUser";
+import NavBarHome from "../../../subComponentes/navBar/NavBarHome";
+import Footer from "../../../subComponentes/footer/Footer";
 
 function DireccionUser() {
   return (
-    <section className="contenedorConfigCuenta flexBox-CG">
-      <SidebarLateral/>
-      <FormDireccionUser/> 
+    <section className="flex-contenedorConfigCuenta">
+      <NavBarHome />
+      <div className="contenedorConfigCuenta flexBox-CG">
+        <SidebarLateral />
+        <FormDireccionUser />
+      </div>
+      <Footer />
     </section>
   );
 }
