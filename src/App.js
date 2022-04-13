ebbfbdb46e7312d5ee75f3cde953713d89e734e8
KaterinTello <u.kate.tello@gmail.com>
2022-04-12 21:59:57 -0500

    Refact: Se elimina las rutas adicionales de ConfiguracionCuenta

--- src/App.js
+++ src/App.js
@@ -9,12 +9,11 @@ import CambiarContrasena from "./componentes/paginas/registro-InicioSesion/conte
 import SobreVeride from "./componentes/paginas/sobreVerede/SobreVerede";
 import CarritoCompras from "./componentes/paginas/carritoCompras/CarritoCompras";
 import Home from "./componentes/paginas/home/Home";
-import CuentaUser from "./componentes/paginas/configuracionCuenta/contenedores/CuentaUser";
-import DireccionUser from "./componentes/paginas/configuracionCuenta/contenedores/DireccionUser";
-import MetodoPagoUser from "./componentes/paginas/configuracionCuenta/contenedores/MetodoPagoUser";
 //navs testing
 import NavBarHome from "./componentes/subComponentes/navBar/NavBarHome";
 import NavBarSobreVeride from "./componentes/subComponentes/navBar/NavBarSobreVeride";
+
+import ConfiguracionCuenta from "./componentes/paginas/configuracionCuenta/contenedores/ConfiguracionCuenta";
 
 
 
@@ -24,12 +23,9 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/Carrito-Compras" element={<CarritoCompras/> } />
-{/* 
-        <Route path="/Configuracion-Cuenta" element={<ConfiguracionCuenta/> } /> */}
+
+        <Route path="/Configuracion-Cuenta" element={<ConfiguracionCuenta/> } />
         <Route path="/Cambiar-Contrasena/:token" element={<CambiarContrasena/>} />
-        <Route path="/Configuracion-Cuenta/Metodo-Pago" element={<MetodoPagoUser/> } />
-        <Route path="/Configuracion-Cuenta/Direccion" element={<DireccionUser/> } />
-        <Route path="/Configuracion-Cuenta/Personal" element={<CuentaUser/> } />
         <Route path="/Recuperar-Contrasena" element={<RecuperarContrasena />} />
         <Route path="/Verificar-Cuenta" element={<VerificarCuentaUsuario />} />
         <Route path="/Verificar-Cuenta/Nuevo-Usuario/:id" element={<VerificarCuentaNuevoUsuario />} />
