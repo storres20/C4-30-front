8bfee72e1248d9a0aeb1ec51f81aeb732bc177a2
Rosamaria Rodriguez <rous9716@gmail.com>
2022-04-19 01:07:41 -0500

    Add: Recuperar contraseña

--- src/componentes/paginas/configuracionCuenta/componentes/SidebarLateral.jsx
+++ src/componentes/paginas/configuracionCuenta/componentes/SidebarLateral.jsx
@@ -5,60 +5,90 @@ import { Link } from "react-router-dom";
 import { FiUser, FiMapPin, FiCreditCard } from "react-icons/fi";
 import { useState } from "react";
 
-function SidebarLateral({setStatus}) {
-
-  const [estadoOrdenes, setEstado] = useState('informacionPersonal');
+function SidebarLateral({ setStatus }) {
+  const [estadoOrdenes, setEstado] = useState("informacionPersonal");
 
   const informacionPersonal = () => {
-    setEstado('informacionPersonal')
-    setStatus('informacionPersonal');
+    setEstado("informacionPersonal");
+    setStatus("informacionPersonal");
   };
 
   const ubicacionEntrega = () => {
-    setEstado('ubicacionEntrega');
-    setStatus('ubicacionEntrega');
+    setEstado("ubicacionEntrega");
+    setStatus("ubicacionEntrega");
   };
 
   const coneccionTarjetas = () => {
-    setEstado('coneccionTarjetas');
-    setStatus('coneccionTarjetas');
+    setEstado("coneccionTarjetas");
+    setStatus("coneccionTarjetas");
   };
-  
-  
+
   return (
     <div className="container-A">
       <div className="titleContainerLeft">
         <h1>Configuración</h1>
       </div>
       <section className="btnsContainer-Configuration">
-        <button onClick={informacionPersonal} id="toDo" className={(estadoOrdenes === 'informacionPersonal') ? 'clicked' : 'btnLeft-CG'} to="/Configuracion-de-Cuenta">
-            <label className="iconbtn-CG">
-              <FiUser className="icon-React" />
-            </label>
-            <aside>
-              <h3>Cuenta</h3>
-              <p>Información Personal</p>
-            </aside>
+        <button
+          onClick={informacionPersonal}
+          id="toDo"
+          className={
+            estadoOrdenes === "informacionPersonal" ? "clicked" : "btnLeft-CG"
+          }
+          to="/Configuracion-de-Cuenta"
+        >
+          <label className="iconbtn-CG">
+            <FiUser className="icon-React" />
+          </label>
+          <aside>
+            <h3>Cuenta</h3>
+            <p>Información Personal</p>
+          </aside>
         </button>
-        <button onClick={ubicacionEntrega} id="toDo" className={(estadoOrdenes === 'ubicacionEntrega') ? 'btnLeft-CG clicked ' : 'btnLeft-CG'} to="/Configuracion-de-Cuenta">
-            <label className="iconbtn-CG">
-              <FiMapPin className="icon-React" />
-            </label>
-            <aside>
-              <h3>Dirección</h3>
-              <p>Ubicación de entrega</p>
-            </aside>
+        <button
+          onClick={ubicacionEntrega}
+          id="toDo"
+          className={
+            estadoOrdenes === "ubicacionEntrega"
+              ? "btnLeft-CG clicked "
+              : "btnLeft-CG"
+          }
+          to="/Configuracion-de-Cuenta"
+        >
+          <label className="iconbtn-CG">
+            <FiMapPin className="icon-React" />
+          </label>
+          <aside>
+            <h3>Dirección</h3>
+            <p>Ubicación de entrega</p>
+          </aside>
         </button>
-        <button onClick={coneccionTarjetas} id="toDo" className={(estadoOrdenes === 'coneccionTarjetas') ? 'btnLeft-CG clicked ' : 'btnLeft-CG'} to="/Configuracion-de-Cuenta">
-            <label>
-              <FiCreditCard className="icon-React" />
-            </label>
-            <aside>
-              <h3>Método de Pago</h3>
-              <p>Conección con Tarjetas</p>
-            </aside>
+        <button
+          onClick={coneccionTarjetas}
+          id="toDo"
+          className={
+            estadoOrdenes === "coneccionTarjetas"
+              ? "btnLeft-CG clicked "
+              : "btnLeft-CG"
+          }
+          to="/Configuracion-de-Cuenta"
+        >
+          <label>
+            <FiCreditCard className="icon-React" />
+          </label>
+          <aside>
+            <h3>Método de Pago</h3>
+            <p>Conección con Tarjetas</p>
+          </aside>
         </button>
       </section>
+      <br />
+      <div className="center">
+        <span> ¿Deseas cambiar contraseña?</span>
+        <span className="forget">
+          <Link to="/Recuperar-Contrasena">Cambiala aquí</Link>
+        </span>
+      </div>
     </div>
   );
 }
