c06b5bfd92e5f055c4e293192d9fd4de44910452
storres20 <sanchotorres777@gmail.com>
2022-04-18 21:29:19 -0500

    add sliderinfinito subcomponent

---
+++ src/componentes/subComponentes/sliderinfinito/SliderInfinito.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import "./sliderinfinito.scss";
+
+export default function SliderInfinito() {
+  return (
+    <div>
+      <div className="bodySlider">
+        <div className="slider">
+          <div className="slide-track">
+            <div className="slide">
+              <img className='slideImg' src="https://i.ibb.co/1X7npWT/promo1.png" height="200" width="550" alt="" />
+            </div>
+            <div className="slide">
+              <img className='slideImg' src="https://i.ibb.co/8dQpZgy/promo2.png" height="200" width="550" alt="" />
+            </div>
+            <div className="slide">
+              <img className='slideImg' src="https://i.ibb.co/nBRcZJd/promo3.png" height="200" width="550" alt="" />
+            </div>
+            {/* <div className="slide">
+              <img className='slideImg' src="https://i.ibb.co/8dQpZgy/promo2.png" height="200" width="550" alt="" />
+            </div> */}
+            {/* <div class="slide">
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+            </div>
+            <div class="slide">
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+            </div> */}
+            {/* <div class="slide">
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+            </div> */}
+            <div className="slide">
+              <img className='slideImg' src="https://i.ibb.co/1X7npWT/promo1.png" height="200" width="550" alt="" />
+            </div>
+            <div className="slide">
+              <img className='slideImg' src="https://i.ibb.co/8dQpZgy/promo2.png" height="200" width="550" alt="" />
+            </div>
+            <div className="slide">
+              <img className='slideImg' src="https://i.ibb.co/nBRcZJd/promo3.png" height="200" width="550" alt="" />
+            </div>
+            {/* <div className="slide">
+              <img className='slideImg' src="https://i.ibb.co/8dQpZgy/promo2.png" height="200" width="550" alt="" />
+            </div> */}
+            {/* <div class="slide">
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/5.png" height="100" width="250" alt="" />
+            </div>
+            <div class="slide">
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/6.png" height="100" width="250" alt="" />
+            </div> */}
+            {/* <div class="slide">
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+            </div> */}
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
